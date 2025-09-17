e4a99abc3c9bc7847d8f3c280493b59a5ace5701
Matty Serwer <Matthew.Serwer@gmail.com>
2025-09-16 23:58:23 -0400

    Initial Spring Boot project setup

---
+++ src/test/java/com/lupinetechnologicdesign/phantompower/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lupinetechnologicdesign.phantompower;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
